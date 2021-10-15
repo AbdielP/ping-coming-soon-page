73c884051d62aaa58247154c9abee6190e5a6ae6
AbdielP <abdiel.pinzonc@gmail.com>
2021-10-14 22:18:43 -0500

    email validation with Vue implemented

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,26 @@
 (() => {
-    const app = {
-        data() {
-            return {
-                name: 'Abdiel P. Carrera'
+    const app = new Vue({
+        el: '#app',
+        data: {
+            error: false,
+            email: null
+        },
+        methods: {
+            checkForm: function (e) {
+                
+                e.preventDefault();
+                if (!this.email) {
+                    this.error = true;
+                } else if (!this.validEmail(this.email)) {
+                    this.error = true;
+                } else {
+                    this.error = false;
+                }
+            },
+            validEmail: function (email) {
+                var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                return re.test(email);
             }
         }
-    }
-    
-    var mountedApp = Vue.createApp(app).mount('#app');
+    })
 })();
